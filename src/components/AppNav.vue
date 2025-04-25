95c8f30f0803f5bb54b55d6a43637a0887157366
EstebanGZam <estebangaviria82@gmail.com>
2025-04-25 02:20:33 -0500

    style: change of title applied

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,7 +1,6 @@
-
 <template>
   <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
-    <a class="navbar-brand" href="#">70d0 0v3r3n61n33r1n6</a>
+    <a class="navbar-brand" href="#">TODOs application</a>
     <ul class="navbar-nav mr-auto">
       <li class="nav-item" v-show="isAdmin()">
         <a class="btn btn-success mr-sm-2" href="#/admin">Admin</a>
@@ -13,30 +12,30 @@
         <a class="btn btn-success mr-sm-2" href="#/login">Login</a>
       </li>
       <li class="nav-item" v-show="isLoggedIn()">
-        <button class="btn btn-danger mr-sm-2" href="#" @click="logout()" >Logout</button>
+        <button class="btn btn-danger mr-sm-2" href="#" @click="logout()">
+          Logout
+        </button>
       </li>
     </ul>
   </nav>
-
 </template>
 
 <script>
-
-import Auth from '@/auth'
+import Auth from "@/auth";
 
 export default {
-  name: 'app-nav',
+  name: "app-nav",
 
   methods: {
-    logout () {
-      Auth.logout()
+    logout() {
+      Auth.logout();
     },
-    isLoggedIn () {
-      return Auth.isLoggedIn()
+    isLoggedIn() {
+      return Auth.isLoggedIn();
     },
-    isAdmin () {
-      return Auth.isAdmin()
-    }
-  }
-}
+    isAdmin() {
+      return Auth.isAdmin();
+    },
+  },
+};
 </script>
