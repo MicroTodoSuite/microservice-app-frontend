d2d28c529abf1d803bdd2f83084d48832cab1ac2
Esteban Gaviria Zambrano <estebangaviria82@gmail.com>
2026-08-16 23:19:19 -0500

    fix: defer backend DNS resolution

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -eu
 
-envsubst '$${AUTH_API_ADDRESS} $${TODOS_API_ADDRESS} $${ZIPKIN_URL}' \
+NGINX_RESOLVER="$(awk '$1 == "nameserver" { print $2; exit }' /etc/resolv.conf)"
+: "${NGINX_RESOLVER:?no DNS resolver found in /etc/resolv.conf}"
+export NGINX_RESOLVER
+
+envsubst '$${AUTH_API_ADDRESS} $${TODOS_API_ADDRESS} $${ZIPKIN_URL} $${NGINX_RESOLVER}' \
   < /etc/nginx/nginx.conf.template \
   > /etc/nginx/nginx.conf
 
